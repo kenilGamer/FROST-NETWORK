c4fe6cae9e462ff48b1b5592640315d28dc15ffb
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-01-23 20:33:42 +0530

    ss

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
-gsap.from('#nav a,img,h1',{
+gsap.from('#nav  a,img,h1',{
     y: -100,
     opacuty: 0,
     duration: 1,
     delay: 0.5,
+    stagger: 0.3
+  })
+  gsap.from('.login',{
+    y: -100,
+    opacuty: 0,
+    duration: 1,
+    delay: 2,
     stagger: 0.3
   })
   

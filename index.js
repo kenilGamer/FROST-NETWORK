8f53008fd0f708ea55da9c3921ac74aceb464231
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-01-26 12:08:32 +0530

    sss

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true,
-});
-
-gsap.registerPlugin(ScrollTrigger);
-
-ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length
-      ? locoScroll.scrollTo(value, 0, 0)
-      : locoScroll.scroll.instance.scroll.y;
-  },
-  getBoundingClientRect() {
-    return {
-      top: 0,
-      left: 0,
-      width: window.innerWidth,
-      height: window.innerHeight,
-    };
-  },
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed",
-});
-
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-ScrollTrigger.refresh();
-
-
 
 const tl = gsap.timeline()
 
